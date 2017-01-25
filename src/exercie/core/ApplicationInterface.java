2f8e5c5366e2c2eb103214da7206d41e1dca4f5e
mathieu becerra <matheiu.becerra.beweb@gmail.com>
2017-01-25 17:51:43 +0100

    Init exo en cours...

---
+++ src/exercie/core/ApplicationInterface.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercie.core;
+
+/**
+ *
+ * @author mathieu-becerra
+ */
+public interface ApplicationInterface {
+
+    public Start(void Start) {
+
+    }
+
+    public Create(void Create) {
+
+    }
+}
